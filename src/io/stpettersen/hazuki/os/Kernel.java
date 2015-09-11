20a9cf39745bfa4b4acdfd94b72d2cbd2727d9a1
Sam Saint-Pettersen <s.stpettersen+github@gmail.com>
2015-09-11 16:36:21 +0100

    ...

---
+++ src/io/stpettersen/hazuki/os/Kernel.java
@@ -0,0 +1,18 @@
+/**
+ * HazukiOS
+ * Copyright 2015 Sam Saint-Pettersen
+ * Released under the MIT/X11 License.
+*/
+package io.stpettersen.hazuki.os;
+
+public class Kernel
+{
+    public Kernel() {
+        System.out.println("HazukiOS");
+        System.out.println("Copyright 2015 Sam Saint-Pettersen");
+    }
+
+    public static void main(String[] args) {
+        Kernel k = new Kernel();
+    }
+}
